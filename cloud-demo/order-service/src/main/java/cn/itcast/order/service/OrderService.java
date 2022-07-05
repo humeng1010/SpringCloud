d3f985aaf4147df7a7821455f326a77382370a1f
小胡 <1589029587@qq.com>
2022-07-05 17:18:38 +0800

    modify project code

--- cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.Objects;
 
 @Service
 public class OrderService {
@@ -22,6 +23,10 @@ public class OrderService {
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
+//        如果没有查询到订单数据则返回null，防止出现空指针异常
+        if(Objects.isNull(order)){
+            return null;
+        }
         // 2.利用RestTemplate发起http请求，查询用户
         String url = "http://userserver/user/"+order.getUserId();
         User user = restTemplate.getForObject(url, User.class);
--- cloud-demo/order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("order")
+@RequestMapping("/order")
 public class OrderController {
 
    @Autowired
